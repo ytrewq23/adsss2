c2a95c60fbaf8ee355ce632c7f634da4c463e993
aizada <ezibaevaa@mail.ru>
2024-04-27 00:17:29 +0500

    interfaces for both classes

---
+++ src/MyList.java
@@ -0,0 +1,23 @@
+public interface MyList<T> extends Iterable<T> {
+    void add(T item);
+    void set(int index, T item);
+    void add(int index, T item);
+    void addFirst(T item);
+    void addLast(T item);
+    T get(int index);
+    T getFirst();
+    T getLast();
+    void remove(int index);
+    void removeFirst();
+    void removeLast();
+    void sort();
+    int indexOf(Object object);
+    int lastIndexOf(Object object);
+    boolean exists(Object object);
+    public Object[] toArray();
+
+    void addElement(T data);
+
+    void clear();
+    int size();
+}
